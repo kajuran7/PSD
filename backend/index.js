a986e10b657e792ea79d0d5a3d9eea321ef9e374
kajuran7 <kajubass@gmail.com>
2024-03-28 14:21:56 +0530

    cros orgin

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser())
 
 app.use(
     cors({
-      origin: ["http://localhost:3000"],
+      origin: ["http://localhost:3000","https://psd-1.onrender.com"],
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
